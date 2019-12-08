999cd3018f9e47cb26b795d1b46687b341cf27b8
Bruno Freitas <bruno_freitas@live.com>
2019-12-08 11:00:18 -0300

    create routers

--- api/routes/api.php
+++ api/routes/api.php
@@ -2,17 +2,23 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+// /*
+// |--------------------------------------------------------------------------
+// | API Routes
+// |--------------------------------------------------------------------------
+// |
+// | Here is where you can register API routes for your application. These
+// | routes are loaded by the RouteServiceProvider within a group which
+// | is assigned the "api" middleware group. Enjoy building your API!
+// |
+// */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('funcionario/list', 'api\FuncionariosController@index');
+Route::get('funcionario/show', 'api\FuncionariosController@show');
+Route::post('funcionario/create', 'api\FuncionariosController@store');
+Route::delete('funcionario/destroy', 'api\FuncionariosController@delete');
+Route::put('funcionario/destroy', 'api\FuncionariosController@update');
