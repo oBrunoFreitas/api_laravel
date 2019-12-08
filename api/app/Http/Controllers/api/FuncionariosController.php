1923e81fd5a9dd19fad82ec4cc7a7688d19eef93
Bruno Freitas <bruno_freitas@live.com>
2019-12-08 11:06:05 -0300

    make funcionariosController

---
+++ api/app/Http/Controllers/api/FuncionariosController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Funcionario;
+
+class FuncionariosController extends Controller
+{
+    public function index()
+    {
+        return Funcionario::all();
+
+    }
+    
+    public function store(Request $request)
+    {
+        $data = Funcionario::create($request->all());
+        return response()->json($data, 201);
+    }
+
+    public function show($id)
+    {
+        return Funcionario::findOrFail($id);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $funcionario = Funcionario::findOrFail($id);
+        $funcionario->update($request-all());
+    }
+
+    public function delete($id)
+    {
+        $funcionario = Funcionario::findOrFail($id);
+        $funcionario->delete();
+    }
+}
